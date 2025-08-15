799f1e929e34a08f3c69a457fcc31e7e6c2dc556
K Kevin Gladson <kevingladson2427@gmail.com>
2025-08-15 20:06:18 +0530

    Porting UserService has been completed

---
+++ UserService/src/main/java/com/kevin/UserService/Controller/UserController.java
@@ -0,0 +1,39 @@
+package com.kevin.UserService.Controller;
+
+import com.kevin.UserService.Model.UserModel;
+import com.kevin.UserService.Services.UserServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserServices userServices;
+
+    @GetMapping("/getUserByEmail")
+    public ResponseEntity<UserModel> getUserByEmailAddress(@RequestParam("email") String email){
+        try{
+            UserModel user = userServices.getUserByEmailAddress(email);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @PostMapping("/addNewUser")
+    public ResponseEntity<UserModel> addNewUser(@RequestBody UserModel user){
+        try{
+            UserModel newUser  = userServices.createNewUser(user);
+            return ResponseEntity.ok(newUser);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+
+
+
+}
