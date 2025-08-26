6c0c6585e73dfb8cb29660cceeb30f825b721441
K Kevin Gladson <kevingladson2427@gmail.com>
2025-08-26 22:31:12 +0530

    Gateway has been done success fully

--- Gateway/src/main/java/com/kevin/Gateway/Config/RouteConfig.java
+++ Gateway/src/main/java/com/kevin/Gateway/Config/RouteConfig.java
@@ -1,42 +1,24 @@
 package com.kevin.Gateway.Config;
 
-import com.kevin.Gateway.handler.BookResponseHandler;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
-
 @Configuration
 @RequiredArgsConstructor
 public class RouteConfig {
 
-    private final BookResponseHandler bookResponseHandler;
-
     @Bean
-    public RouterFunction<ServerResponse> bookRouter() {
-        return route()
-                // GET all books
-                .GET("/books/getAllBooks", accept(MediaType.APPLICATION_JSON), bookResponseHandler::getAllBooks)
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("book-service", r -> r.path("/api/v1/books/**")
+                        .uri("http://localhost:5010"))
+                .route("user-service", r -> r.path("/api/v1/users/**")
+                        .uri("lb://USER-SERVICE"))
 
-                // GET books by name/ibsno
-                .GET("/books/getBooks", accept(MediaType.APPLICATION_JSON), bookResponseHandler::getBooks)
-
-                // POST add book
-                .POST("/books/addBook", accept(MediaType.MULTIPART_FORM_DATA), bookResponseHandler::addBook)
-
-                // DELETE book (use query param for ID)
-                .DELETE("/books/delete", bookResponseHandler::deleteBook)
-
-                // POST update book
-                .POST("/books/update", accept(MediaType.APPLICATION_JSON), bookResponseHandler::updateBook)
                 .build();
     }
-
-
 }
+
